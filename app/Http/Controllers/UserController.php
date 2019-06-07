2ca7092e1e1dec5b1802c940a1f92f0373b15f89
leidyRA <50849650+leidyRA@users.noreply.github.com>
2019-06-07 09:54:32 -0500

    Autentication
    user and roles Autentication

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdminController extends Controller{
+class UserController extends Controller
+{
     function index(){
         
-        return view('admin.principal');
+        return view('user.principal');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,29 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware'=>'web'], function () {
+    Auth::routes();
+    Route::get('/',function(){
+        return redirect()->guest('login');
+    });
+   
+});
+
+Route::group(['prefix'=>'admin','middleware'=>['web','auth','admin']],
+    function(){
+        Route::get('/',['uses'=>'AdminController@index','as'=>'admin.index']);
+        
+        
+});
+Route::group(['prefix'=>'user','middleware'=>['web','auth','user']],
+    function(){
+        Route::get('/',['uses'=>'UserController@index','as'=>'user.index']);
+        
+        
+});
 
 Route::get('/', 'WelcomeController@index');
-Route::get('/admin', 'AdminController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
