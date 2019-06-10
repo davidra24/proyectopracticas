4fe8478f1a327fb2860cc56040145f6b5b723945
leidyRA <50849650+leidyRA@users.noreply.github.com>
2019-06-10 00:30:16 -0500

    change model viewPractice

--- app/viewPractice.php
+++ app/viewPractice.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class viewPractice extends Model
 {
-    protected $fillable = ['id','place', 'date_practice','name','conductor','type'];
+    protected $fillable = ['id','id_teacher','id_bus','place', 'date_practice','name','conductor','type'];
     
 }
